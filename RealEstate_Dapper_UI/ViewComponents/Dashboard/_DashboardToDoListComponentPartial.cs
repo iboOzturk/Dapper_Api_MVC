4148643438068c72b2c667fe0424b43c01a6062a
iboOzturk <ozturk.ibo@outlook.com>
2023-12-16 15:31:16 +0300

    Dashboard ekranında todolist alanı düzenlendi.

---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardToDoListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.ToDoListDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashboardToDoListComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        string url = "https://localhost:44383/api/ToDoLists/";
+
+        public _DashboardToDoListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()   
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync(url);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultToDoListDto>>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+    }
+}
